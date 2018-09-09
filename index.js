83b2ce9e7628072d39fbd1c9a61d63591361efce
Nikolay <nrf-224@mail.ru>
2018-09-09 19:57:03 +0300

    Deleted all, created application from scratch

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
-'use strict';
+'use strict'
 
-let express = require('express');
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
 
-let app = express();
+const ROOT = path.join(__dirname, 'files')
 
-let handlebars = require('express-handlebars').create({
-  defaultLayout:'main',
-  extname: '.hbs'
-});
+http.createServer((req, res) => {
+  const filePath = url.parse(req.url).pathname
 
-app.engine('hbs', handlebars.engine);
-app.set('view engine', 'hbs');
+  switch (req.method) {
+    case 'GET':
+      if (req.url === '/') {
+        res.end('Please enter the filename in adress line')
+      } else {
+        sendFile(filePath, res)
+      };
+  };
+}).listen(3000)
 
-app.set('port', process.env.PORT || 3000);
+function sendFile (filePath, res) {
+  // отдачу файлов следует переделать "правильно", через потоки, с нормальной обработкой ошибок
+  fs.readFile(ROOT + filePath, (err, content) => {
+    if (err) {
+      res.statusCode = 404
+      res.end('404 - Not found')
+    } else {
+      res.setHeader('Content-Type', checkExtention(filePath))
+      res.end(content)
+    }
+  })
+};
 
-app.use(express.static(__dirname + '/files'));
+function checkExtention (filePath) {
+  switch (path.extname(filePath)) {
+    case '.txt':
+      return 'text/plain;charset=utf-8'
+    default:
+      return 'text/html;charset=utf-8'
+  }
+};
 
-app.get('/', function(req, res){
-  res.render('index');
-});
+/*
+Алгоритм работы сервера:
+1. Сервер получает get-запрос, в URL которого название запрашиваемого файла
+2. Сервер проверяет, есть ли такой файл в директории files
+3. Если файл есть, то сервер возвращает 200 статус и передаёт файл
+4. Если файла нет, то сервер возвращает 404 статус и отображает сообщение "Файл %FILENAME% не найден"
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started at http://localhost:' +
-  app.get('port') + '; press Ctrl+C to interrupt.' );
-});
+TO DO:
+1. Переписать с использованием async/await
+2. Сделать метод, обрабатывающий отправку файлов POST-запросом
+3. Сделать метод, обрабатывающий удаление файлов DELETE-запросом
+4. Сделать веб-форму, позволяющую увидеть все файлы, загрузить новые и удалить существующие
+*/
